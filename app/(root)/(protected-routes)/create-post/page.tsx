33f7561ac4b1171b8079d6dc9e975f540357faa8
islam-ahsanul <ahsanulislam.info@gmail.com>
2024-12-11 20:04:52 +0600

    updated the schema

--- app/(root)/(protected-routes)/create-post/page.tsx
+++ app/(root)/(protected-routes)/create-post/page.tsx
@@ -1,9 +1,11 @@
 import PageWrapper from '@/components/common/PageWrapper';
-
+import CreatePostForm from '@/components/create-post/CreatePostForm';
 const CreatePostPage = () => {
 	return (
 		<PageWrapper>
-			<div className="flex flex-row h-full w-full gap-4 items-center justify-center flex-grow">form here</div>
+			<div className="flex flex-row h-full w-full gap-4 items-center justify-center flex-grow">
+				<CreatePostForm />
+			</div>
 		</PageWrapper>
 	);
 };
---
+++ app/api/create-post/route.ts
@@ -0,0 +1,5 @@
+import { NextResponse } from 'next/server';
+import { db } from '@/lib/db';
+import { getUserByEmail } from '@/actions/auth-actions/authAction';
+
+export const POST = async (req: Request) => {};
